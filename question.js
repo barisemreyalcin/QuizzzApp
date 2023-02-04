55200a5c0c91a8e3df4ae6a944ca90f9b3faada1
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-02-04 15:43:00 +0300

    Time remaining info added

--- question.js
+++ question.js
@@ -10,5 +10,7 @@ Question.prototype.answerCheck = function(answer) {
 
 let questions = [
     new Question("1- Which one is JavaScript Package Management application?", {A: "Node.js", B: "npm", C: "TypeScript", D: "PHP"}, "B"),
-    new Question("2- Which one is .NET Package Management application?", {A: "Node.js", B: "npm", C: "NuGet", D: "PHP"}, "C")
+    new Question("2- Which one is .NET Package Management application?", {A: "Node.js", B: "npm", C: "NuGet", D: "PHP"}, "C"),
+    new Question("3- Which one is not a must for Frontend Development?", {A: "HTML", B: "Node.js", C: "CSS", D: "JavaScipt"}, "B"),
+    new Question("4- Which one is a Javascript framework?", {A: "React", B: "SCSS", C: "ASP.NET", D: "Django"}, "A")
 ];
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const ui = new UI();
 ui.btn_start.addEventListener("click", function() {
     
     ui.quiz_box.classList.add("active");
+    startTimer(15);
     ui.displayQuestion(quiz.bringQuestion());
     ui.displayQuestionNo(quiz.questionIndex + 1, quiz.questions.length);
 
@@ -12,11 +13,13 @@ ui.btn_start.addEventListener("click", function() {
 ui.btn_next.addEventListener("click", function() {
     if(quiz.questions.length != quiz.questionIndex +1) {
         quiz.questionIndex +=1;
+        clearInterval(counter);
+        startTimer(15);
         ui.displayQuestion(quiz.bringQuestion());
         ui.displayQuestionNo(quiz.questionIndex + 1, quiz.questions.length);
         ui.btn_next.classList.remove("show")
     } else {
-        console.log("Questions are over");
+        clearInterval(counter);
         ui.quiz_box.classList.remove("active");
         ui.score_box.classList.add("active");
         ui.displayScore(quiz.questions.length, quiz.correctAnswerNo);
@@ -34,6 +37,7 @@ ui.btn_restart.addEventListener("click", function() {
 });
 
 function optionSelected(option) {
+    clearInterval(counter);
     let answer = option.querySelector("span b").textContent;
     let question = quiz.bringQuestion();
 
@@ -50,4 +54,32 @@ function optionSelected(option) {
         ui.option_list.children[i].classList.add("disabled");
     }
     ui.btn_next.classList.add("show")
+}
+
+let counter;
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+
+    function timer() {
+        ui.time_second.textContent = time;
+        time--;
+
+        if(time < 0) {
+            clearInterval(counter);
+
+            ui.time_text.textContent = "Time is up";
+
+            let answer = quiz.bringQuestion().answerCorrect;
+
+            for(let option of ui.option_list.children) {
+
+                if(option.querySelector("span b").textContent == answer) {
+                    option.classList.add("correct");
+                    option.insertAdjacentHTML("beforeend", ui.correctIcon);
+                }
+                option.classList.add("disabled");
+            }
+            ui.btn_next.classList.add("show");
+        } 
+    }
 }
